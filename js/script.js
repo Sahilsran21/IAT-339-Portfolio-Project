84e041f923e5891f2768bfcb7aadf6f4015d6d1c
Sahilsran21 <sahilsran21@gmail.com>
2022-12-07 13:30:13 -0800

    Commit of Dec 7 - TA Feedback Corrections
    Added links to each image on Home Page that directs user to specific post on Instagram. Added a Works Page that houses Projects 1 and 2, and updated navigation header to reflect this change. Footer contact link has been shortened to just the email. Feedback page has semi-functional pop-up alert when submit button is clicked, and an alternative back image.  For About Page, simplified the text for both Youtube/Instagram links. For both project pages, included information about where the projects can be found at the end of the pages.

--- js/script.js
+++ js/script.js
@@ -7,39 +7,3 @@
      hamburgerMenu.classList.toggle('bx-x');
      navigationBar.classList.toggle('open');
    }
-
-
-
-   const cartButton=document.querySelector(".form_button");
-   const alertBox=document.querySelector(".alert_box");
-   const closeButton=document.querySelector(".close_alert_button")
-   let timer;
-
-
-
-   cartButton.addEventListener("click",function () {
-        showAlertBox();
-   })
-
-   closeButton.addEventListener("click",function () {
-        hideAlertBox();
-        clearTimeout(timer);
-   })
-
-   function showAlertBox(){
-      alertBox.classList.remove("hide");
-      alertBox.classList.add("show");
-      // hide animation onload
-
-      if(alertBox.classList.contains("hidden")){
-          alertBox.classList.remove("hidden");
-      }
-      timer=setTimeout(function(){
-          hideAlertBox();
-      },5000)
-   }
-
-   function hideAlertBox(){
-     alertBox.classList.remove("show");
-      alertBox.classList.add("hide");
-   }
